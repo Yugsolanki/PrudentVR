e542d9ba517362433c6a32c92233fdaafc960c14
Yug Solanki <yugsolanki15@gmail.com>
2023-07-25 23:50:17 +0530

    turning is working, made it class based

--- helper.js
+++ helper.js
@@ -1,386 +1,146 @@
 import * as THREE from "three";
 
-/**
- * Check if two cylinders are intersecting
- * @param {THREE.Mesh} mesh1
- * @param {THREE.Mesh} mesh2
- * @returns {boolean}
- * @private
- */
-export const checkIntersectionBetweenCylinder = (mesh1, mesh2) => {
-  let position1 = mesh1.position;
-  let position2 = mesh2.position;
+class Helpers {
 
-  let distance = position1.manhattanDistanceTo(position2);
-  console.log(distance);
-  let diamter = 10;
-
-  return distance <= diamter;
-};
-
-export const checkIntersectingSegments = (segments, mesh) => {
-  for (let [index, segment] of segments.entries()) {
-    let position1 = segment.position;
-    let position2 = mesh.position;
-
-    const dx = position2.x - position1.x;
-    const dy = position2.y - position1.y;
-    const dz = position2.z - position1.z;
-
-    // const distance = Math.abs(dx * dx) + Math.abs(dy * dy) + Math.abs(dz * dz);
-    const distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
-    // let distance = position1.distanceTo(position2);
-    if (distance <= 10) {
-      return [true, index];
+  /**
+   * Gives a array of radiuses that layers of the wood will have.
+   * @param {Number} woodLayersCount Number of layers of the wood
+   * @param {Number} startRadius Radius of the first/outer layer of the wood
+   * @returns Array of radiuses
+   */
+  getLayersRadius(woodLayersCount = 15, startRadius = 5) {
+    const woodLayersRadius = [startRadius];
+    const Dx = woodLayersRadius[0] / woodLayersCount;
+    for (let i = 1; i < woodLayersCount; i++) {
+      woodLayersRadius[i] = woodLayersRadius[i - 1] - Dx;
     }
+    return woodLayersRadius;
   }
-  return [false, null];
-};
 
-export const checkIntersectionWithBoundingBox = (boundingBox, mesh) => {
-  const cylinderPosition = mesh.position;
-  const boundingBoxCenter = new THREE.Vector3();
-  boundingBox.getCenter(boundingBoxCenter);
-  const distance = cylinderPosition.distanceTo(boundingBoxCenter);
 
-  const radius = boundingBox.getSize(new THREE.Vector3()).x / 2;
-  return distance <= radius;
-};
+  /**
+   * Gives if chisel and bounding box are intersecting
+   * @param {THREE.Mesh} chisel
+   * @param {THREE.Mesh} boundingBox
+   * @returns
+   */
+  getCollisionAndDepth(chisel, boundingBox) {
+    const height = chisel.geometry.parameters.height;
+    const position = chisel.position.clone();
+    const chiselTop = new THREE.Vector3(
+      position.x,
+      position.y + height / 2,
+      position.z
+    );
 
-/**
- * Render a wooden object
- * @private
- * @returns {void}
- */
-export const renderWood = (scene) => {
-  let y = -10;
-  let segmentCount = 100;
-  let widthOfEachSegment = 20 / segmentCount;
-  let segments = [];
-  for (let i = 0; i < segmentCount; i++) {
-    let geometry = new THREE.CylinderGeometry(5, 5, widthOfEachSegment);
-    let material = new THREE.MeshBasicMaterial({
-      color: 0x00ff00,
-      wireframe: true,
-    });
-    let cylinder = new THREE.Mesh(geometry, material);
-    cylinder.position.set(y, 0, 0);
-    cylinder.rotation.z = Math.PI / 2;
-    scene.add(cylinder);
-    y += widthOfEachSegment;
-    segments.push(cylinder);
+    const position2 = boundingBox.position.clone();
+
+    const comparision = 5.1;
+    let depth = 0;
+    let distance = position2.y - chiselTop.y;
+
+    if (distance <= comparision) {
+      depth = comparision - distance;
+      return [true, distance, depth];
+    }
+
+    return [false, distance, depth];
   }
-  return segments;
-};
 
-/**
- * Render a cylinder using THREE.BufferGeometry()
- * @private
- * @returns {void}
- * @see https://threejs.org/docs/#api/en/core/BufferGeometry
- * @see https://threejs.org/docs/#api/en/core/BufferAttribute
- */
-export const renderBufferGeometryCylinder = () => {
-  const radiusTop = 5;
-  const radiusBottom = 5;
-  const height = 20;
-  const radialSegments = 32;
-  const heightSegments = 1;
-  const openEnded = false;
 
-  const geometry = new THREE.BufferGeometry();
+  /**
+   * Gets you the wood segments between the top left vertex and the top right vertex of the chisel which needs to be deleted
+   * @param {Array<THREE.Mesh>} segments
+   * @param {THREE.Mesh} chisel
+   * @returns starting segment and ending segment number which need to be deleted
+   */
+  getSegments(segments, chisel) {
+    const [topLeftVertex, topRightVertex, bottomLeftVertex, bottomRightVertex] = this.getCylinderEndVertices(chisel);
 
-  const vertexCount = (radialSegments + 1) * (heightSegments + 1);
-  const indexCount = radialSegments * heightSegments * 2 * 3;
+    let topLeft = new THREE.Vector3(topLeftVertex[0],topLeftVertex[1],topLeftVertex[2]);
+    let topRight = new THREE.Vector3(topRightVertex[0],topRightVertex[1],topRightVertex[2]);
 
-  const positions = new Float32Array(vertexCount * 3);
-  const indices = new Uint32Array(indexCount);
+    let startSegmentIndex = 0, endSegmentIndex = 0;
+    const comparision = 0.1;
 
-  const positionAttribute = new THREE.BufferAttribute(positions, 3);
-  const indexAttribute = new THREE.BufferAttribute(indices, 1);
-
-  geometry.setAttribute("position", positionAttribute);
-  geometry.setIndex(indexAttribute);
-
-  let positionIndex = 0;
-  let index = 0;
-
-  for (let y = 0; y <= heightSegments; y++) {
-    const v = y / heightSegments;
-    const radius = v * (radiusBottom - radiusTop) + radiusTop;
-
-    for (let x = 0; x <= radialSegments; x++) {
-      const u = x / radialSegments;
-      const theta = u * Math.PI * 2;
-
-      const xPos = radius * Math.cos(theta);
-      const yPos = -height / 2 + v * height;
-      const zPos = radius * Math.sin(theta);
-
-      positions[positionIndex++] = xPos;
-      positions[positionIndex++] = yPos;
-      positions[positionIndex++] = zPos;
-
-      if (x < radialSegments && y < heightSegments) {
-        const i = x + y * (radialSegments + 1);
-
-        indices[index++] = i;
-        indices[index++] = i + 1;
-        indices[index++] = i + radialSegments + 1;
-
-        indices[index++] = i + radialSegments + 1;
-        indices[index++] = i + 1;
-        indices[index++] = i + radialSegments + 2;
+    for (let [index, segment] of segments.entries()) {
+      const distance = this.getDistanceBetweenTwoPoints(segment.position,topLeft);
+      if (distance <= comparision) {
+        startSegmentIndex = index;
+        break;
       }
     }
-  }
 
-  const material = new THREE.MeshBasicMaterial({
-    color: 0xffff00,
-    wireframe: true,
-  });
-  const cylinder = new THREE.Mesh(geometry, material);
-
-  scene.add(cylinder);
-
-  console.log(cylinder.geometry.attributes.position.array);
-};
-
-export const getCylinderEndVertices = (cylinder) => {
-  const position = cylinder.position.clone();
-  const height = cylinder.geometry.parameters.height;
-  const rotation = cylinder.rotation;
-  const radius = cylinder.geometry.parameters.radiusTop;
-
-  const topVertex = new THREE.Vector3(0, height / 2, 0)
-    .applyEuler(rotation)
-    .add(position);
-  const bottomVertex = new THREE.Vector3(0, -height / 2, 0)
-    .applyEuler(rotation)
-    .add(position);
-
-  const vertices = [
-    [topVertex.x - radius, topVertex.y, topVertex.z],
-    [topVertex.x + radius, topVertex.y, topVertex.z],
-    [bottomVertex.x - radius, bottomVertex.y, bottomVertex.z],
-    [bottomVertex.x + radius, bottomVertex.y, bottomVertex.z],
-  ];
-
-  return vertices;
-};
-
-export const deleteSegmentsBetweenVertices = (segments, vertices) => {
-  //   let [topVertex1, topVertex2, bottomVertex1, bottomVertex2] = vertices;
-  let [topVertex, bottomVertex] = [vertices[0], vertices[1]];
-
-  let topVertex1 = new THREE.Vector3(...topVertex);
-  let topVertex2 = new THREE.Vector3(...topVertex);
-  let bottomVertex1 = new THREE.Vector3(...bottomVertex);
-  let bottomVertex2 = new THREE.Vector3(...bottomVertex);
-
-  topVertex1.x -= 5;
-  topVertex2.x += 5;
-  bottomVertex1.x -= 5;
-  bottomVertex2.x += 5;
-
-  let topSegmentIndex = null;
-  let bottomSegmentIndex = null;
-
-  for (let [index, segment] of segments.entries()) {
-    let position1 = segment.position;
-    let position2 = topVertex1;
-
-    const dx = position2.x - position1.x;
-    const dy = position2.y - position1.y;
-    const dz = position2.z - position1.z;
-
-    const distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
-    // let distance = position1.distanceTo(position2);
-    if (distance <= 10) {
-      topSegmentIndex = index;
-      break;
+    for (let i = segments.length - 1; i >= 0; i--) {
+      const distance = this.getDistanceBetweenTwoPoints(segments[i].position,topRight);
+      if (distance <= comparision) {
+        endSegmentIndex = i;
+        break;
+      }
     }
+
+    return [startSegmentIndex, endSegmentIndex];
   }
 
-  for (let [index, segment] of segments.entries()) {
-    let position1 = segment.position;
-    let position2 = topVertex2;
-
-    const dx = position2.x - position1.x;
-    const dy = position2.y - position1.y;
-    const dz = position2.z - position1.z;
-
-    const distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
-    // let distance = position1.distanceTo(position2);
-    if (distance <= 10) {
-      topSegmentIndex = index;
-      break;
-    }
-  }
-
-  for (let [index, segment] of segments.entries()) {
-    let position1 = segment.position;
-    let position2 = bottomVertex1;
-
-    const dx = position2.x - position1.x;
-    const dy = position2.y - position1.y;
-    const dz = position2.z - position1.z;
-
-    const distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
-    // let distance = position1.distanceTo(position2);
-    if (distance <= 10) {
-      bottomSegmentIndex = index;
-      break;
-    }
-  }
-
-  for (let [index, segment] of segments.entries()) {
-    let position1 = segment.position;
-    let position2 = bottomVertex2;
-
-    const dx = position2.x - position1.x;
-    const dy = position2.y - position1.y;
-    const dz = position2.z - position1.z;
-
-    const distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
-
-    if (distance <= 10) {
-      bottomSegmentIndex = index;
-      break;
-    }
-  }
-
-  if (topSegmentIndex !== null && bottomSegmentIndex !== null) {
-    segments.splice(topSegmentIndex, bottomSegmentIndex - topSegmentIndex);
-  }
-
-  return segments;
-};
-
-export const temp = (segments, vertices) => {
-  const [topLeftVertex, topRightVertex, bottomLeftVertex, bottomRightVertex] =
-    vertices;
-
-  let topSegmentIndex = null;
-  let bottomSegmentIndex = null;
-
-  for (let [index, segment] of segments.entries()) {
-    let position1 = segment.position;
-    let position2 = topLeftVertex;
-
-    const dx = position2.x - position1.x;
-    const dy = position2.y - position1.y;
-    const dz = position2.z - position1.z;
-
-    const distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
-    // const distance = Math.abs(dx * dx) + Math.abs(dy * dy) + Math.abs(dz * dz);
-
-    // let distance = position1.distanceTo(position2);
-    if (distance <= 0.5) {
-      topSegmentIndex = index;
-      break;
-    }
-  }
-
-  for (let [index, segment] of segments.entries()) {
-    let position1 = segment.position;
-    let position2 = topRightVertex;
-
-    const dx = position2.x - position1.x;
-    const dy = position2.y - position1.y;
-    const dz = position2.z - position1.z;
-
-    const distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
-    // const distance = Math.abs(dx * dx) + Math.abs(dy * dy) + Math.abs(dz * dz);
-
-    // let distance = position1.distanceTo(position2);
-    if (distance <= 0.5) {
-      topSegmentIndex = index;
-      break;
-    }
-  }
-
-  for (let [index, segment] of segments.entries()) {
-    let position1 = segment.position;
-    let position2 = bottomLeftVertex;
-
-    const dx = position2.x - position1.x;
-    const dy = position2.y - position1.y;
-    const dz = position2.z - position1.z;
-
-    const distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
-    // const distance = Math.abs(dx * dx) + Math.abs(dy * dy) + Math.abs(dz * dz);
-    // let distance = position1.distanceTo(position2);
-    if (distance <= 0.5) {
-      bottomSegmentIndex = index;
-      break;
-    }
-  }
-
-  for (let [index, segment] of segments.entries()) {
-    let position1 = segment.position;
-    let position2 = bottomRightVertex;
-
-    const dx = position2.x - position1.x;
-    const dy = position2.y - position1.y;
-    const dz = position2.z - position1.z;
-
-    const distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
-    // const distance = Math.abs(dx * dx) + Math.abs(dy * dy) + Math.abs(dz * dz);
-
-    if (distance <= 0.5) {
-      bottomSegmentIndex = index;
-      break;
-    }
-  }
-
-  if (topSegmentIndex !== null && bottomSegmentIndex !== null) {
-    segments.splice(topSegmentIndex, bottomSegmentIndex - topSegmentIndex + 1);
-  }
-  // console.log(segments);
-  return segments;
-};
-
-export const getSegments = (segments, vertices) => {
-  const [topLeftVertex, topRightVertex, bottomLeftVertex, bottomRightVertex] = vertices;
-  let topLeft = new THREE.Vector3(topLeftVertex[0], topLeftVertex[1], topLeftVertex[2]);
-  let newSegments = [];
-
-  let startSegmentIndex = null;
-  let endSegmentIndex = null;
-
-  // Find the segment closest to the top left vertex
-  for (let [index, segment] of segments.entries()) {
-    const position = segment.position;
-    const dx = position.x - topLeft.x;
-    const dy = position.y - topLeft.y-5;
-    const dz = position.z - topLeft.z;
-
-    const distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
-
-    if (distance <= 0.1) {
-      startSegmentIndex = index;
-      break;
-    }
-  }
-
-  // // Find the end segment based on width calculations
-  const totalSegments = segments.length;
-  const widthOfEachSegment = 20 / totalSegments;
-  let start = 0;
-
-  for (let i = startSegmentIndex; i < segments.length; i++) {
-    const segment = segments[i];
-    const end = start + widthOfEachSegment;
-
-    if (end >= 2) {
-  		endSegmentIndex = i;
-  		break;
-    }
-    start += widthOfEachSegment;
-  }
-
-  // // Copy the selected segments to the newSegments array
+  /**
+   * Gives the end vertices of a cylinder
+   * @param {THREE.Mesh} cylinder 
+   * @returns Position of 4 visible vertices of the cylinder
+   */
+  getCylinderEndVertices = (cylinder) => {
+    const position = cylinder.position.clone();
+    const height = cylinder.geometry.parameters.height;
+    const rotation = cylinder.rotation;
+    const radius = cylinder.geometry.parameters.radiusTop;
   
-  return [startSegmentIndex, endSegmentIndex+1];
-};
+    const topVertex = new THREE.Vector3(0, height / 2, 0)
+      .applyEuler(rotation)
+      .add(position);
+    const bottomVertex = new THREE.Vector3(0, -height / 2, 0)
+      .applyEuler(rotation)
+      .add(position);
+  
+    const vertices = [
+      [topVertex.x - radius, topVertex.y, topVertex.z],
+      [topVertex.x + radius, topVertex.y, topVertex.z],
+      [bottomVertex.x - radius, bottomVertex.y, bottomVertex.z],
+      [bottomVertex.x + radius, bottomVertex.y, bottomVertex.z],
+    ];
+  
+    return vertices;
+  };
+
+  /**
+   * Gets you the distance between two Three.Vector3 points
+   * @param {THREE.Vector3} point1 
+   * @param {THREE.Vector3} point2 
+   * @returns distance between two points
+   */
+  getDistanceBetweenTwoPoints(point1, point2) {
+    const dx = point1.x - point2.x;
+    // const dy = point1.y - point2.y;
+    const dz = point1.z - point2.z;
+    
+    const distance = Math.abs(dx * dx)  + Math.abs(dz * dz);
+    return distance;
+  }
+
+  /**
+   * Generates an array of hsl values with of shades of brown going from dark to light
+   * @param {Number} numShades 
+   * @returns array of hsl values
+   */
+  generateShadesOfBrown(numShades) {
+    const shades = [];
+    const baseHue = 30; // Brown hue in HSL color model
+    for (let i = 1; i <= numShades; i++) {
+      const lightness = (100 / numShades) * i;
+      const color = `hsl(${baseHue}, 100%, ${lightness}%)`;
+      shades.push(color);
+    }
+    return shades;
+  }
+  
+}
+
+export default Helpers;
--- main.js
+++ main.js
@@ -1,90 +1,165 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import {renderWood, checkIntersectingSegments, checkIntersectionWithBoundingBox,temp,getSegments, checkIntersectionBetweenCylinder, getCylinderEndVertices} from "./helper.js";
+import Helpers from "./helper";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.set(0, 0, 20);
-camera.lookAt(0, 0, 0);
+class WoodTurningMachine {
+  constructor() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    this.camera.position.set(0, 0, 15);
+    this.camera.lookAt(0, 0, 0);
 
+    this.helpers = new Helpers();
 
-//Wood
-let segments = renderWood(scene);
+    this.keyCodes = [37,38,39,40,87,83];
+    this.chiselSpeed = 0.1
+    this.woodLayersCount = 20;
+    this.woodLayersRadius = this.helpers.getLayersRadius();
+    this.layerColor = this.helpers.generateShadesOfBrown(this.woodLayersCount);
 
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
 
-// Create a chisel type tool
-const geometry4 = new THREE.CylinderGeometry(1, 1, 10);
-const material4 = new THREE.MeshBasicMaterial({
-  color: 0x00ffff,
-  wireframe: true,
-});
-const cylinder2 = new THREE.Mesh(geometry4, material4);
-cylinder2.position.set(0, -14, 0);
-scene.add(cylinder2);
-const bla = getCylinderEndVertices(cylinder2);
-console.log(bla);
+    this.orbit = new OrbitControls(this.camera, this.renderer.domElement);
+    this.camera.position.set(0, 0, 20);
+    this.orbit.update();
 
+    this.woodLayers = this.renderWood()
+    this.invisibleCylinder = this.renderBoundingBox()
 
-// Create a bounding box
-const boundingBox = new THREE.Box3();
-const radius = segments[0].geometry.parameters.radiusTop;
-const height = 20;
-boundingBox.setFromCenterAndSize(new THREE.Vector3(0,0,0), new THREE.Vector3(radius, height, radius));
-boundingBox.applyMatrix4(new THREE.Matrix4().makeRotationZ(Math.PI/2));
-const [boxMin, boxMax] = [boundingBox.min, boundingBox.max];
+    this.chiselMesh = this.renderChisel();
+    this.scene.add(this.chiselMesh)
 
+    //Start animation
+    this.animate()
+  }
+
+  /**
+   * Changes the position of the chisel
+   * @param {Number} key Key Number which is being pressed
+   */
+  chiselMovement(key) {
+    if (this.keyCodes.includes(key)) 
+      this.temp(); 
+
+    if (key == 37) 
+      this.chiselMesh.position.x -= this.chiselSpeed; //left
+    else if (key == 39) 
+      this.chiselMesh.position.x += this.chiselSpeed; //right
+    else if (key == 38)
+      this.chiselMesh.position.y += this.chiselSpeed; //up
+    else if (key == 40) 
+      this.chiselMesh.position.y -= this.chiselSpeed; //down
+    else if (key == 87) 
+      this.chiselMesh.position.z += this.chiselSpeed; //w
+    else if (key == 83)
+      this.chiselMesh.position.z -= this.chiselSpeed; //s
+  }
+ 
+  /**
+   * Render woods
+   * @param {Number} height height of the wood
+   * @param {Number} segmentCount number of segments in wood
+   * @param {Number} woodLayersCount number of layers to wood
+   * @returns woodLayers an array, all layers of wood
+   */
+  renderWood(height = 20, segmentCount = 100, woodLayersCount = 15) {
+    const widthOfEachSegment = height / segmentCount;
+    const woodSegments = [];
+    const woodLayers = [];
+    const woodLayersRadius = this.woodLayersRadius;
+
+    for (let i = 0; i < woodLayersCount; i++) {
+      woodSegments.length = 0;
+      let x = -height / 2;
+      for (let j = 0; j < segmentCount; j++) {
+        let geometry = new THREE.CylinderGeometry(
+          woodLayersRadius[i],
+          woodLayersRadius[i],
+          widthOfEachSegment
+        );
+
+        let color = this.layerColor[i];
+      
+        let material = new THREE.MeshBasicMaterial({color: color,wireframe: false});
+        let cylinder = new THREE.Mesh(geometry, material);
+        cylinder.position.set(x, 0, 0);
+        cylinder.rotation.z = Math.PI / 2;
+        this.scene.add(cylinder);
+        x += widthOfEachSegment;
+        woodSegments.push(cylinder);
+      }
+      woodLayers[i] = [...woodSegments];
+    }
+    return woodLayers;
+  }
+
+  /**
+   * Gives us a Bounding Box for wood
+   * @param {Number} radius radius of the bounding box
+   * @param {Number} height height of the bounding box
+   * @returns gives a bounding box
+   */
+  renderBoundingBox(radius = 5, height = 10, ) {
+    const boundingBoxGeometry = new THREE.CylinderGeometry(radius, radius, height, 32);
+    const boundingBoxMaterial = new THREE.MeshBasicMaterial({
+      transparent: true,
+      opacity: 0,
+    });
+    const boundingBoxMesh = new THREE.Mesh(boundingBoxGeometry, boundingBoxMaterial);
+    return boundingBoxMesh;
+  }
+
+  /**
+   * Render Chisel
+   * @param {Number} radius radius of the chisel
+   * @param {Number} height height of the chisel
+   * @returns chisel mesh
+   */
+  renderChisel(radius = 1, height = 5) {
+    const chiselGeometry = new THREE.CylinderGeometry(radius,radius,height,32);
+    const chiselMaterial = new THREE.MeshBasicMaterial({
+      color: 0xffff00,
+      wireframe: true,
+    });
+    const chiselMesh = new THREE.Mesh(chiselGeometry, chiselMaterial);
+    chiselMesh.position.set(0, -10, 0);
+    return chiselMesh;
+  }
+
+  
+  animate() {
+    requestAnimationFrame(this.animate.bind(this));
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  /**
+   * It will check if collision is happening or not and if collision is happening then it will get the segments to be deleted from the wood layer and delete it from the scene
+   */
+  temp() {
+    const [bool, distance, depth] = this.helpers.getCollisionAndDepth(
+      this.chiselMesh,
+      this.invisibleCylinder
+    );
+    if (bool) {
+      this.chiselSpeed = 0.025
+      for (let i = 0; i<this.woodLayersCount; i++) {
+        if (distance < this.woodLayersRadius[i]) {
+          const [start, end] = this.helpers.getSegments(this.woodLayers[i], this.chiselMesh);
+          for (let j = start; j <= end; j++) {
+            this.scene.remove(this.woodLayers[i][j]);
+          }
+        }
+      }
+    }
+  }
+
+}
+
+const WoodTurning = new WoodTurningMachine();
 
 //Add eventlister to move the tool
 document.addEventListener("keydown", function (event) {
-  if (event.keyCode == 37) {
-    //left
-    cylinder2.position.x -= 0.1;
-  } else if (event.keyCode == 39) {
-    //right
-    cylinder2.position.x += 0.1;
-  } else if (event.keyCode == 38) {
-    //up
-    cylinder2.position.y += 0.1;
-  } else if (event.keyCode == 40) {
-    //down
-    cylinder2.position.y -= 0.1;
-  } else if (event.keyCode == 87) {
-    //w
-    cylinder2.position.z += 0.1;
-  } else if (event.keyCode == 83) {
-    //s
-    cylinder2.position.z -= 0.1;
-  }
+  WoodTurning.chiselMovement(event.keyCode);
 });
-
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const orbit = new OrbitControls(camera, renderer.domElement);
-camera.position.set(0, 0, 20);
-orbit.update();
-
-
-function animate() {
-  requestAnimationFrame(animate);
-  // cylinder.rotation.x += 0.01;
-
-  // const isIntersecting = checkIntersectionWithBoundingBox(boundingBox, cylinder2);
-
-  const [isIntersecting2, index] = checkIntersectingSegments(segments, cylinder2);
-
-  
-  if (isIntersecting2) {
-    console.log("Intersecting");
-  }
-
-  renderer.render(scene, camera);
-}
-animate();
